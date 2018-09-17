34cdeb0fc0d8b4f0de2fee33e4c236ebd253ff73
Jonatha Lima <devjonh@gmail.com>
2018-09-17 00:59:21 -0300

    Add carthage libraries and add viewcoding protocol

---
+++ OurWedding/Common/Protocols/ViewCodingProtocol.swift
@@ -0,0 +1,27 @@
+//
+//  ViewCodingProtocol.swift
+//  OurWedding
+//
+//  Created by Jonatha Lima on 17/09/18.
+//  Copyright © 2018 Sunshine Consulting. All rights reserved.
+//
+
+import SnapKit
+
+protocol ViewCodingProtocol {
+    func setupViewCode()
+    func addSubviews()
+    func setupConstraints()
+    func setupSubviews()
+}
+
+extension ViewCodingProtocol {
+    
+    func setupViewCode() {
+        addSubviews()
+        setupConstraints()
+    }
+    
+    func setupSubviews() {}
+    
+}
--- OurWedding/Feature/Login/Controller/LoginViewController.swift
+++ OurWedding/Feature/Login/Controller/LoginViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    private var loginViewScreen: LoginViewScreen
+    
+    init() {
+        loginViewScreen = LoginViewScreen()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        self.view = loginViewScreen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
