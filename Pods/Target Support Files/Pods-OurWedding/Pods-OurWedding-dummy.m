31fdcd2e5834731ec4ad13ece1c1cd065da9f849
Jonatha Lima <devjonh@gmail.com>
2018-09-19 01:58:16 -0300

    Remove Pods and Carthage files

--- Pods/Target Support Files/Pods-OurWedding/Pods-OurWedding-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OurWedding : NSObject
-@end
-@implementation PodsDummy_Pods_OurWedding
-@end
--- Pods/Target Support Files/Pods-OurWeddingUITests/Pods-OurWeddingUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OurWeddingUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OurWeddingUITests
-@end
